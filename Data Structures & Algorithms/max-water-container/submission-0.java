234ccf47c16489a5941d305c849340ead934f0e6
lokyaan <149294365+lokyaan@users.noreply.github.com>
2026-07-27 23:27:32 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int l=0;
+        int r=heights.length -1;
+        int max=0;
+        while(l<r){
+            int width= r-l;
+            int height= Math.min(heights[l],heights[r]);
+            max=Math.max(max,width*height);
+            if(heights[l]<=heights[r]) l++;
+            else r--;
+
+        }
+        return max;
+    }
+}
