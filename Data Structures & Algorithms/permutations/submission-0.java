70de4a48dd2390cefb2df54b6aff465fe08763f6
lokyaan <149294365+lokyaan@users.noreply.github.com>
2026-07-28 16:10:51 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result= new ArrayList<>();
+        boolean used[]= new boolean[nums.length];
+        permutations(nums,used, new ArrayList<>(), result);
+        return result;
+        
+    }
+    public static void permutations(int nums[], boolean used[],List<Integer> curr, List<List<Integer>> result){
+        if(curr.size()== nums.length){
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            curr.add(nums[i]);
+            used[i]=true;
+            permutations(nums,used,curr,result);
+            curr.remove(curr.size()-1);
+            used[i]=false;
+        }
+    }
+}
