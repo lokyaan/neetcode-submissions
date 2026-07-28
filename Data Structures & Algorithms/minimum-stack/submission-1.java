df422cfee657ed81d6cc0f41eca968b2f9460d68
lokyaan <149294365+lokyaan@users.noreply.github.com>
2026-07-28 19:07:26 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,35 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack= new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+       stack.push(val);
+       if(minStack.isEmpty() || minStack.peek()>=val){
+        minStack.push(val);
+       }
+        
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty()){
+            if(stack.peek().equals(minStack.peek())){
+                minStack.pop();
+            }
+            stack.pop();
+        }
+        
+    }
+    
+    public int top() {
+      return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+        
+    }
+}
